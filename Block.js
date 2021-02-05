4b841053cdc0279979d1ffc675f6b41f1c725c83
Nithurshen <75428875+Nithurshen@users.noreply.github.com>
2021-02-05 16:45:16 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,51 @@
+class Block{
+
+    constructor(x, y, width, height){
+
+        var options = {
+           
+            restitution : 0.4,
+            friction : 0.0,
+           
+        }
+
+        this.visibility = 225;
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        this.image = loadImage("monster.png");
+
+        World.add(world, this.body);
+        
+      }
+     
+      display(){
+
+        if(this.body.speed < 3){
+
+        var pos = this.body.position;
+
+        push();
+
+        translate(pos.x, pos.y);
+        imageMode(CENTER);
+        image(this.image, 0, 0, this.width, this.height);
+
+        pop();
+
+      }
+      
+      else{
+
+        World.remove(world, this.body);
+
+        push();
+
+        this.visibility = this.visibility -5;
+
+        pop();
+        
+      }
+
+    }
+}
